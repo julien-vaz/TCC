b854bac597a7964103c26ac9e44d5a1c25ed41f1
julien-vaz <jhmvaz95@gmail.com>
2023-09-14 19:43:21 -0300

    Crossover implemented

--- seamo2.py
+++ seamo2.py
@@ -1,5 +1,7 @@
 import initial_population_generator as ipg
 import parser
+from random import choice
+from copy import deepcopy
 
 
 class SEAMO2:
@@ -8,7 +10,7 @@ class SEAMO2:
         self.demand_matrix = parser.DemandMatrixParser("MandlDemand.txt")
         self.initial_population_generator = ipg.InitialPopulationGenerator(transport_network)
 
-    def get_shortest_path_travel_time(routeset, origin_id, destination_id):
+    def get_shortest_path_travel_time(self, routeset, origin_id, destination_id):
         shortest_path_travel_time = "Infinite"
         for route in routeset:
             if origin_id in route and destination_id in route:
@@ -28,11 +30,11 @@ class SEAMO2:
                     shortest_path_travel_time = path_travel_time
         return shortest_path_travel_time
 
-    def calculate_passenger_cost(routeset_size, routeset, demand_matrix):
+    def calculate_passenger_cost(self, routeset_size, routeset, demand_matrix):
         numerator = 0
         for i in range(len(routeset_size)):
             for j in range(len(routeset_size)):
-                shortest_path_travel_time_from_i_to_j = get_shortest_path_travel_time(routeset, i, j)
+                shortest_path_travel_time_from_i_to_j = self.get_shortest_path_travel_time(routeset, i, j)
                 partial_numerator = demand_matrix[i][j] * shortest_path_travel_time_from_i_to_j
                 numerator += partial_numerator
         denominator = 0
@@ -42,13 +44,44 @@ class SEAMO2:
         passenger_cost = numerator / denominator
         return passenger_cost
 
-    def calculate_operator_cost(routeset):
+    def calculate_operator_cost(self, routeset):
         operator_cost = 0
         for route in routeset:
             for i, j in route:
                 for link in self.transport_network.graph[i].links:
                     if link[0] == j:
                         operator_cost += link[1]
+
+    def crossover(self, parent1, parent2):
+        offspring = []
+        parent1_copy = deepcopy(parent1)
+        parent2_copy = deepcopy(parent2)
+        seed = set(choice(parent1_copy))
+        parent1_copy.remove(seed)
+        offspring.append(seed)
+        first_parent = True
+        while len(offspring) < len(parent1):
+            if first_parent:
+                chosen_parent = parent2_copy
+            else:
+                chosen_parent = parent1_copy
+            potential_routes = []
+            for route in chosen_parent:
+                if not seed.isdisjoint(set(route)):
+                    potential_routes.append(route)
+            next_route = []
+            smallest_intersection = len(self.transport_network.graph)
+            for route in potential_routes:
+                intersection = seed.intersection(set(route))
+                if intersection < smallest_intersection:
+                    smallest_intersection = intersection
+                    next_route = route
+                    chosen_parent.remove(next_route)
+                    offspring.append(next_route)
+            seed.add(set(next_route))
+            first_parent = not first_parent
+        return offspring
+            
 
 
 seamo2 = SEAMO2()
@@ -73,3 +106,10 @@ for routeset in seamo2.initial_population_generator.population:
     operator_cost.append(routeset_operator_cost)
     if routeset_operator_cost == min(operator_cost):
         best_routeset_so_far_operator_cost = routeset
+
+generations = int(input("How many generations do you want? "))
+for _ in range(generations):
+    for parent1 in seamo2.initial_population_generator.population:
+        parent2 = random.choice(seamo2.initial_population_generator)
+        if parent1 != parent2:
+            offspring = crossover(parent1, parent2)
